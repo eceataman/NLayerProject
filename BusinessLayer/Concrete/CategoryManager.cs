5f9e636fb9c273f560997b77f8f2fe7ac7d40197
Ece Ataman <114909014+eceataman@users.noreply.github.com>
2024-10-30 00:08:00 +0300

    business layer eklendi

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        public Category GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Category t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/FluentValidation/ProductValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.FluentValidation
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator() {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("ürün adını bos geçemezsiniz");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("ürün adı minimum 3 karakter olmalıdır");
+            RuleFor(x => x.Stock).NotEmpty().WithMessage("stok sayısı bos gecilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("fiyat bos gecilemez");
+        
+        }
+    }
+}

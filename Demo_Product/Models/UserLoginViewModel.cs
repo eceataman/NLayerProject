7693123addd7395a33c7098b7b87a27dd9555262
Ece Ataman <114909014+eceataman@users.noreply.github.com>
2024-12-15 17:33:05 +0300

    login seçenegi eklendi

---
+++ Demo_Product/Models/UserLoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
+{
+    public class UserLoginViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı adı giriniz")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Şifre giriniz")]
+        public string Password { get; set; }
+    }
+}
